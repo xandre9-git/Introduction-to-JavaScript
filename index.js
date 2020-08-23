8de0c79cea97fbf2dd220dca054f1d4477fb1a61
xandre9-git <jmjames673@gmail.com>
2020-08-23 09:00:18 -0400

    completed dogyears problem

--- index.js
+++ index.js
@@ -93,13 +93,14 @@ console.log(dogFeeder(5, 0.75));
 
 
 
-
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
 // Your function should take a string (either rock paper or sissors)
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+
+
 
   
   
