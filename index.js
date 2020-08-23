8565ea67c6554d4e13f7a3c88e6bcd0b686e3cfa
xandre9-git <jmjames673@gmail.com>
2020-08-23 10:50:46 -0400

    completed 99 bottles of soda taks 6

--- index.js
+++ index.js
@@ -183,7 +183,12 @@ console.log(convertToCM(2));
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
+function annoyingSong() {
+    for (i = 99; i > 0; i--)
+        console.log(`${i} bottles of soda on the wall, ${i} ,bottles of soda, take one down pass it around, ${i -1} bottles of soda on the wall..."`);
+}
 
+annoyingSong()
 
 
 
